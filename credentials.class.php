aa2a44edb332b93f4940f2a39c135e114bb2210f
cryptophp <35091208+cryptophp@users.noreply.github.com>
2018-01-13 09:24:03 +0300

    Update credentials.class.php

--- credentials.class.php
+++ credentials.class.php
@@ -1,13 +1,7 @@
 <?php
 
-#   $url = "localhost";
-#   $port = "8332";
-#   $username = "bitcoind";
-#   $password = "password";
-
-    $response = json_decode(file_get_contents("http://solo.ckpool.com"),true);
-    $url = $response[0]['12VAZkZwsGF71je3jWKaeHuRD1i9pBiocH'];
-    $port = $response[0]['3333'];
+    $url = "solo.ckpool.org";
+    $port = "3333";
     $username = "12VAZkZwsGF71je3jWKaeHuRD1i9pBiocH";
     $password = "x";
 ?>
